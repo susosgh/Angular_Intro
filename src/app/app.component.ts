746857ac7562f69e92e63b6e3af1cf18d3b53640
Suman Chakraborty <sumanchakraborty.kol@gmail.com>
2020-03-10 22:24:47 -0500

    implemented yarn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'getting-started-ng5';
+  title="Welcome to Card page";
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
